b487c39c751f8b25f89c8fde0ac940fe4eefba34
Your Name <thiagoeu@gmail.com>
2016-11-14 14:28:21 -0200

    Started integration with elasticsearch

--- app/Movie.php
+++ app/Movie.php
@@ -7,11 +7,12 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use Laravel\Scout\Searchable;
 
 class Movie extends Model
 {
 
-    use SoftDeletes;
+    use SoftDeletes, Searchable;
 
     protected $table = 'movies';
 
@@ -53,7 +54,7 @@ class Movie extends Model
      */
     public static function getOrCreate($external_url)
     {
-        if(self::prepare($external_url) === false) {
+        if (self::prepare($external_url) === false) {
             throw new \Exception('Link IMDb/TMDb inválido.');
         }
 
@@ -61,51 +62,51 @@ class Movie extends Model
             $external_id = ExternalId::whereIn('external_id', [self::$tmdb_id, self::$imdb_id])->firstOrFail();
 
             return $external_id->movie;
-        } catch(ModelNotFoundException $e) {
+        } catch (ModelNotFoundException $e) {
             $movie_data = self::getMovieData();
             $genres = [];
 
-            foreach($movie_data->genres as $genre) {
+            foreach ($movie_data->genres as $genre) {
                 $genres[] = $genre->name;
             }
 
-            if(isset($movie_data->release_date)) {
+            if (isset($movie_data->release_date)) {
                 $year = explode('-', $movie_data->release_date)[0];
             } else {
                 $year = explode('-', $movie_data->first_air_date)[0];
             }
 
-            if(isset($movie_data->original_title)) {
+            if (isset($movie_data->original_title)) {
                 $slug = Str::slug($movie_data->original_title);
-                if($slug === '') {
+                if ($slug === '') {
                     $slug = Str::slug($movie_data->title);
                 }
             } else {
                 $slug = Str::slug($movie_data->original_name);
-                if($slug === '') {
+                if ($slug === '') {
                     $slug = Str::slug($movie_data->name);
                 }
             }
 
             $movie = new Movie([
-                'title'          => (isset($movie_data->title)) ? $movie_data->title : $movie_data->name,
+                'title' => (isset($movie_data->title)) ? $movie_data->title : $movie_data->name,
                 'original_title' => (isset($movie_data->original_title)) ? $movie_data->original_title : $movie_data->original_name,
-                'year'           => $year,
-                'slug'           => $slug,
-                'category'       => self::$category,
-                'genre'          => implode(',', $genres),
-                'description'    => $movie_data->overview,
-                'poster_path'    => (isset($movie_data->poster_path)) ? $movie_data->poster_path : null,
-                'backdrop_path'  => (isset($movie_data->backdrop_path)) ? $movie_data->backdrop_path : null,
+                'year' => $year,
+                'slug' => $slug,
+                'category' => self::$category,
+                'genre' => implode(',', $genres),
+                'description' => $movie_data->overview,
+                'poster_path' => (isset($movie_data->poster_path)) ? $movie_data->poster_path : null,
+                'backdrop_path' => (isset($movie_data->backdrop_path)) ? $movie_data->backdrop_path : null,
             ]);
             $movie->save();
 
             $external_ids = [];
-            if(self::$tmdb_id !== null) {
+            if (self::$tmdb_id !== null) {
                 $external_ids[] = ['name' => 'tmdb', 'external_id' => self::$tmdb_id, 'movie_id' => $movie->id];
             }
 
-            if(self::$imdb_id !== null) {
+            if (self::$imdb_id !== null) {
                 $external_ids[] = ['name' => 'imdb', 'external_id' => self::$imdb_id, 'movie_id' => $movie->id];
             }
 
@@ -121,14 +122,14 @@ class Movie extends Model
      */
     private static function prepare($external_url)
     {
-        if(preg_match('/(tt\d{7})/', $external_url, $matches) !== 0) {
+        if (preg_match('/(tt\d{7})/', $external_url, $matches) !== 0) {
             self::$imdb_id = $matches[1];
             self::$external_provider = 'imdb';
 
             return true;
         }
 
-        if(preg_match('/(movie|tv)\/(\d{1,6})/', $external_url, $matches) !== 0) {
+        if (preg_match('/(movie|tv)\/(\d{1,6})/', $external_url, $matches) !== 0) {
             self::$category = $matches[1];
             self::$tmdb_id = $matches[2];
             self::$external_provider = 'tmdb';
@@ -147,10 +148,10 @@ class Movie extends Model
         /**
          * Necessary if user uses IMDb URL.
          */
-        if(self::$external_provider === 'imdb') {
+        if (self::$external_provider === 'imdb') {
             $imdb_result = self::callCurl('https://api.themoviedb.org/3/find/' . self::$imdb_id . '?external_source=imdb_id&api_key=' . env('TMDB_API_KEY'));
 
-            if(count($imdb_result->movie_results) > 0) {
+            if (count($imdb_result->movie_results) > 0) {
                 self::$category = 'movie';
                 self::$tmdb_id = $imdb_result->movie_results[0]->id;
             } else {
@@ -171,7 +172,7 @@ class Movie extends Model
         $tmdb_result_en = self::callCurl('http://api.themoviedb.org/3/' . self::$category . '/' . self::$tmdb_id . '?api_key=' . env('TMDB_API_KEY'));
 
         // Merging data
-        return (object)array_merge(array_filter((array)$tmdb_result_en), array_filter((array)$tmdb_result_pt));
+        return (object) array_merge(array_filter((array) $tmdb_result_en), array_filter((array) $tmdb_result_pt));
     }
 
     /**
@@ -182,7 +183,7 @@ class Movie extends Model
     {
         $ch = curl_init($url);
         curl_setopt_array($ch, [
-            CURLOPT_POST           => false,
+            CURLOPT_POST => false,
             CURLOPT_RETURNTRANSFER => true,
         ]);
         $result = json_decode(curl_exec($ch));
